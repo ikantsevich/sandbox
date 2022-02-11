b12049dec7c403476e6e5a4b1e1bba13fc5f0416
izzatdev <toshpulatovizzatulla@gmail.com>
2022-02-11 13:52:44 +0500

    added removed things,tested, finished.

--- src/main/java/com/exadel/sandbox/exception/EntityNotFoundException.java
+++ src/main/java/com/exadel/sandbox/exception/EntityNotFoundException.java
@@ -1,7 +1,15 @@
 package com.exadel.sandbox.exception;
 
 public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException() {
+        super();
+    }
+
     public EntityNotFoundException(String message) {
         super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
