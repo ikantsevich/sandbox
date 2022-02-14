124e66f688d967bf3c42f41ac34bc8732ba6b668
Asilbek Abdurashidov <asilbekdev@gmail.com>
2022-02-14 07:57:41 +0500

    Added KeyboardService to support more telegram functionality.

---
+++ src/main/java/com/exadel/sandbox/bot/service/impl/KeyboardServiceImpl.java
@@ -0,0 +1,107 @@
+package com.exadel.sandbox.bot.service.impl;
+
+import com.exadel.sandbox.bot.service.KeyboardService;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class KeyboardServiceImpl implements KeyboardService {
+    @Override
+    public InlineKeyboardMarkup getInlineKeyboard(List<List<String>> name, List<List<String>> callbackData) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> inlineKeyboard = new ArrayList<>();
+
+        for (int i = 0; i < name.size(); i++) {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            for (int j = 0; j < name.get(i).size(); j++) {
+                InlineKeyboardButton button = new InlineKeyboardButton();
+                button.setText(name.get(i).get(j));
+                button.setCallbackData(callbackData.get(i).get(j));
+                row.add(button);
+            }
+            inlineKeyboard.add(row);
+        }
+
+        inlineKeyboardMarkup.setKeyboard(inlineKeyboard);
+
+        return inlineKeyboardMarkup;
+    }
+
+    @Override
+    public InlineKeyboardMarkup getInlineKeyboard(List<List<String>> name, List<List<String>> callbackData, List<List<String>> urls) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> inlineKeyboard = new ArrayList<>();
+
+        for (int i = 0; i < name.size(); i++) {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            for (int j = 0; j < name.get(i).size(); j++) {
+                InlineKeyboardButton button = new InlineKeyboardButton();
+                button.setText(name.get(i).get(j));
+                button.setCallbackData(callbackData.get(i).get(j));
+                if (urls.get(i).get(j) != null)
+                    button.setUrl(urls.get(i).get(i));
+                row.add(button);
+            }
+            inlineKeyboard.add(row);
+        }
+
+        inlineKeyboardMarkup.setKeyboard(inlineKeyboard);
+
+        return inlineKeyboardMarkup;
+    }
+
+    @Override
+    public ReplyKeyboardMarkup getReplyKeyboard(List<List<String>> buttons) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        List<KeyboardRow> rows = new ArrayList<>();
+
+        for (List<String> button : buttons) {
+            KeyboardRow keyboardRow = new KeyboardRow();
+            for (String s : button) keyboardRow.add(s);
+            rows.add(keyboardRow);
+        }
+
+        replyKeyboardMarkup.setKeyboard(rows);
+
+        return replyKeyboardMarkup;
+    }
+
+    @Override
+    public ReplyKeyboardMarkup getReplyKeyboard(List<List<String>> buttons,
+                                                List<List<Boolean>> requestContact) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = getReplyKeyboard(buttons);
+        List<KeyboardRow> keyboard = replyKeyboardMarkup.getKeyboard();
+
+        for (int i = 0; i < buttons.size(); i ++) {
+            KeyboardRow keyboardButtons = keyboard.get(i);
+            for (int j = 0; j < buttons.get(i).size(); j++)
+                keyboardButtons.get(j).setRequestContact(requestContact.get(i).get(j));
+        }
+        return replyKeyboardMarkup;
+    }
+
+    @Override
+    public ReplyKeyboardMarkup getReplyKeyboard(List<List<String>> buttons,
+                                                List<List<Boolean>> requestContact,
+                                                List<List<Boolean>> requestLocation) {
+        ReplyKeyboardMarkup keyboardMarkup = getReplyKeyboard(buttons, requestContact);
+        List<KeyboardRow> keyboard = keyboardMarkup.getKeyboard();
+
+        for (int i = 0; i < buttons.size(); i ++) {
+            KeyboardRow keyboardButtons = keyboard.get(i);
+            for (int j = 0; j < buttons.get(i).size(); j++)
+                keyboardButtons.get(j).setRequestLocation(requestLocation.get(i).get(j));
+        }
+
+        return keyboardMarkup;
+    }
+}
--- src/main/java/com/exadel/sandbox/bot/service/impl/MessageHandlerImpl.java
+++ src/main/java/com/exadel/sandbox/bot/service/impl/MessageHandlerImpl.java
@@ -1,6 +1,8 @@
 package com.exadel.sandbox.bot.service.impl;
 
 import com.exadel.sandbox.bot.feign.TelegramFeign;
+import com.exadel.sandbox.bot.service.BotService;
+import com.exadel.sandbox.bot.service.KeyboardService;
 import com.exadel.sandbox.bot.service.MessageHandler;
 import com.exadel.sandbox.bot.utils.TelegramUtils;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,23 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class MessageHandlerImpl implements MessageHandler {
     private final TelegramFeign telegramFeign;
+    private final KeyboardService keyboardService;
 
+
+//    This place is just for testing for not. Then there are going to be a logic
     @Override
     public void handle(Message message) {
-        SendMessage sendMessage = new SendMessage(message.getChatId().toString(), message.getText());
-
-        telegramFeign.sendMessage(TelegramUtils.TOKEN, sendMessage);
+        System.out.println(message.getText());
+        if (message.getText() != null) {
+            SendMessage sendMessage = new SendMessage(message.getChatId().toString(), message.getText());
+            sendMessage.setReplyMarkup(keyboardService.getReplyKeyboard(List.of(List.of("Share contact", "Play")), List.of(List.of(true, false))));
+            telegramFeign.sendMessage(TelegramUtils.TOKEN, sendMessage);
+        }
     }
 }
