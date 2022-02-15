9f9b74903561921dbb920c80117fabc74cf406e6
Your Name <you@example.com>
2022-02-15 10:33:46 +0500

    em_notif notification tables crud

--- src/main/java/com/exadel/sandbox/seat/repository/SeatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exadel.sandbox.seat.repository;
-
-import com.exadel.sandbox.seat.entity.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeatRepository extends JpaRepository<Seat, Long> {
-}
