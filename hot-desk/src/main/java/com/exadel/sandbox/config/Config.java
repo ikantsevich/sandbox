8578d7a7f4cd90c5d95cda8a1f087453dbb80f6e
Asilbek Abdurashidov <asilbekdev@gmail.com>
2022-02-16 01:12:43 +0500

    Modified Dependencies and implemented swagger

--- hot-desk/src/main/java/com/exadel/sandbox/config/Config.java
+++ hot-desk/src/main/java/com/exadel/sandbox/config/Config.java
@@ -6,11 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+@EnableSwagger2
 public class Config {
 
     @Bean
@@ -24,5 +30,14 @@ public class Config {
     @Bean
     AuditorAware<String> auditorProvider() {
         return Optional::empty;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
