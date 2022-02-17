675ee021fb624f3f1b970f05b2ce169cc5780b65
User <zoyirovj@mail.ru>
2022-02-18 03:23:12 +0500

    deleted notif and em_notif

--- src/main/java/com/exadel/sandbox/em_notif/repository/EmplyeeNotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exadel.sandbox.em_notif.repository;
-
-import com.exadel.sandbox.em_notif.entity.EmployeeNotification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmplyeeNotificationRepository extends JpaRepository<EmployeeNotification, Long> {
-}
