b4a6455cbac99724cb9bb24749ef85b76dfcb0de
Saidamin Qambarov <saidaminqambarov408@gmail.com>
2022-02-22 03:22:25 +0500

    security part is added

--- src/main/java/com/exadel/sandbox/equipment/dto/EquipmentUpdateDto.java
+++ src/main/java/com/exadel/sandbox/equipment/dto/EquipmentUpdateDto.java
@@ -6,8 +6,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class EquipmentUpdateDto extends EquipmentBaseDto{
 
