af787b0745190eeaa8e2965ac258c4538728aa2d
Saidamin Qambarov <saidaminqambarov408@gmail.com>
2022-03-02 19:18:56 +0500

    some changes

--- telegram-bot/src/main/java/com/exadel/telegrambot/bot/utils/Urls.java
+++ telegram-bot/src/main/java/com/exadel/telegrambot/bot/utils/Urls.java
@@ -1,15 +1,9 @@
 package com.exadel.telegrambot.bot.utils;
 
 public interface Urls {
-
     String TOKEN="bot"+ Security.TOKEN +"/";
     String TELEGRAM_BASE="https://api.telegram.org/";
-
     String BASE_WEBHOOK="api/telegram";
     String FULL_REQUEST=TELEGRAM_BASE+TOKEN;
-
-
-
     String GLOBAL="https://06a6-178-218-201-90.ngrok.io/";
-
 }
