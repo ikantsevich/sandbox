0a4a71d2db03e936fe5cf09ee9dfc95d45a7eaee
Saidamin Qambarov <saidaminqambarov408@gmail.com>
2022-03-14 14:15:04 +0500

    some changes

--- hot-desk/src/main/java/com/exadel/sandbox/officeFloor/controller/OfficeController.java
+++ hot-desk/src/main/java/com/exadel/sandbox/officeFloor/controller/OfficeController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.ws.rs.Path;
 import java.time.LocalDate;
 import java.util.List;
 
